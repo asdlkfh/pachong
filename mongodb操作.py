dbe49fdcd35d4b4a449df81509111da9a4396f36
zhangchanglong <zhangchanglong@stu.xjtu.edu.cn>
2022-05-27 18:39:56 +0800

    bbb

--- mongodb操作.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-import pymysql
-client = pymongo.MongoClient(host = 'localhost',port = 27017)
-db = client.test
-collection = db.test1
-dict1 = {'aa':1, 'bb':2, 'cc':3}
-collection.insert(dict1)
